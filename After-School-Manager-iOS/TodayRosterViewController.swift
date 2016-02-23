080ab9ee0127ca55c2e5d016402e8b55d930966f
sand3rs0n <sanderson@msserv.com>
2016-02-23 16:32:35 -0500

    Adds SignedOuts to list
    Separates signed out and signed in students in todays rosters and sign
    out list into two sections

--- After-School-Manager-iOS/TodayRosterViewController.swift
+++ After-School-Manager-iOS/TodayRosterViewController.swift
@@ -17,6 +17,7 @@ class TodayRosterViewController: UIViewController, UITableViewDataSource, UITabl
     private var forwardedRosterID = 0
     private var forwardedStudentFirstName = ""
     private var forwardedStudentLastName = ""
+    private var numberOfNonSignedOut = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,7 +35,8 @@ class TodayRosterViewController: UIViewController, UITableViewDataSource, UITabl
         let month = date.getCurrentMonth()
         let day = date.getCurrentDay()
         let weekday = date.getCurrentWeekday()
-        let querySQL = "SELECT STUDENTROSTERS.studentFirstName AS studentFirstName, STUDENTROSTERS.studentLastName AS studentLastName, STUDENTROSTERS.studentID AS studentID, STUDENTROSTERS.rosterID AS rosterID, ROSTERS.name AS name FROM STUDENTROSTERS LEFT OUTER JOIN ROSTERS ON STUDENTROSTERS.rosterID = ROSTERS.rosterID  WHERE (endYear > '\(year)' OR (endYear = '\(year)' AND endMonth > '\(month)') OR (endYear = '\(year)' AND endMonth = '\(month)' AND endDay >= '\(day)')) AND (startYear < '\(year)' OR (startYear = '\(year)' AND startMonth < '\(month)') OR (startYear = '\(year)' AND startMonth = '\(month)' AND startDay <= '\(day)')) AND \(weekday) = 1  AND studentID IN (SELECT studentID FROM STUDENTPROFILES WHERE active = 1) UNION SELECT STUDENTPROFILES.firstName AS studentFirstName, STUDENTPROFILES.lastName AS studentLastName, STUDENTPROFILES.studentID AS studentID, ROSTERS.rosterID as rosterID, ROSTERS.name as name FROM ONETIMEATTENDANCE LEFT OUTER JOIN STUDENTPROFILES ON ONETIMEATTENDANCE.studentID = STUDENTPROFILES.studentID LEFT OUTER JOIN ROSTERS ON ONETIMEATTENDANCE.rosterID = ROSTERS.rosterID WHERE year = '\(year)' AND month = '\(month)' AND day = '\(day)' AND active = 1 ORDER BY studentLastName, studentFirstName, name ASC"
+        let querySQL = "SELECT STUDENTROSTERS.studentFirstName AS studentFirstName, STUDENTROSTERS.studentLastName AS studentLastName, STUDENTROSTERS.studentID AS studentID, STUDENTROSTERS.rosterID AS rosterID, ROSTERS.name AS name FROM STUDENTROSTERS LEFT OUTER JOIN ROSTERS ON STUDENTROSTERS.rosterID = ROSTERS.rosterID  WHERE (endYear > '\(year)' OR (endYear = '\(year)' AND endMonth > '\(month)') OR (endYear = '\(year)' AND endMonth = '\(month)' AND endDay >= '\(day)')) AND (startYear < '\(year)' OR (startYear = '\(year)' AND startMonth < '\(month)') OR (startYear = '\(year)' AND startMonth = '\(month)' AND startDay <= '\(day)')) AND \(weekday) = 1  AND studentID IN (SELECT studentID FROM STUDENTPROFILES WHERE active = 1) AND studentID NOT IN (SELECT studentID FROM SIGNOUTS WHERE day = '\(day)' AND month = '\(month)' AND year = '\(year)') UNION SELECT STUDENTPROFILES.firstName AS studentFirstName, STUDENTPROFILES.lastName AS studentLastName, STUDENTPROFILES.studentID AS studentID, ROSTERS.rosterID as rosterID, ROSTERS.name as name FROM ONETIMEATTENDANCE LEFT OUTER JOIN STUDENTPROFILES ON ONETIMEATTENDANCE.studentID = STUDENTPROFILES.studentID LEFT OUTER JOIN ROSTERS ON ONETIMEATTENDANCE.rosterID = ROSTERS.rosterID WHERE year = '\(year)' AND month = '\(month)' AND day = '\(day)' AND active = 1 AND ONETIMEATTENDANCE.studentID NOT IN (SELECT studentID FROM SIGNOUTS WHERE day = '\(day)' AND month = '\(month)' AND year = '\(year)') ORDER BY studentLastName, studentFirstName, name ASC"
+        let signedOutSQL = "SELECT STUDENTROSTERS.studentFirstName AS studentFirstName, STUDENTROSTERS.studentLastName AS studentLastName, STUDENTROSTERS.studentID AS studentID, STUDENTROSTERS.rosterID AS rosterID, ROSTERS.name AS name FROM STUDENTROSTERS LEFT OUTER JOIN ROSTERS ON STUDENTROSTERS.rosterID = ROSTERS.rosterID  WHERE (endYear > '\(year)' OR (endYear = '\(year)' AND endMonth > '\(month)') OR (endYear = '\(year)' AND endMonth = '\(month)' AND endDay >= '\(day)')) AND (startYear < '\(year)' OR (startYear = '\(year)' AND startMonth < '\(month)') OR (startYear = '\(year)' AND startMonth = '\(month)' AND startDay <= '\(day)')) AND \(weekday) = 1  AND studentID IN (SELECT studentID FROM STUDENTPROFILES WHERE active = 1) AND studentID IN (SELECT studentID FROM SIGNOUTS WHERE day = '\(day)' AND month = '\(month)' AND year = '\(year)') UNION SELECT STUDENTPROFILES.firstName AS studentFirstName, STUDENTPROFILES.lastName AS studentLastName, STUDENTPROFILES.studentID AS studentID, ROSTERS.rosterID as rosterID, ROSTERS.name as name FROM ONETIMEATTENDANCE LEFT OUTER JOIN STUDENTPROFILES ON ONETIMEATTENDANCE.studentID = STUDENTPROFILES.studentID LEFT OUTER JOIN ROSTERS ON ONETIMEATTENDANCE.rosterID = ROSTERS.rosterID WHERE year = '\(year)' AND month = '\(month)' AND day = '\(day)' AND active = 1 AND ONETIMEATTENDANCE.studentID IN (SELECT studentID FROM SIGNOUTS WHERE day = '\(day)' AND month = '\(month)' AND year = '\(year)') ORDER BY studentLastName, studentFirstName, name ASC"
 
         let results = database.search(querySQL)
         while (results.next()) {
@@ -47,12 +49,30 @@ class TodayRosterViewController: UIViewController, UITableViewDataSource, UITabl
             studentList.append(cur)
         }
         results.close()
+        numberOfNonSignedOut = studentList.count
+        let results2 = database.search(signedOutSQL)
+        while (results2.next()) {
+            let cur = StudentRoster()
+            cur.setStudentFirstName(results2.stringForColumn("studentFirstName"))
+            cur.setStudentLastName(results2.stringForColumn("studentLastName"))
+            cur.setStudentID(Int(results2.intForColumn("studentID")))
+            cur.setRosterID(Int(results2.intForColumn("rosterID")))
+            cur.setRosterName(results2.stringForColumn("name"))
+            studentList.append(cur)
+        }
+        results2.close()
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let student = studentList[indexPath.row]
         let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
-        cell.textLabel?.text = student.getStudentFirstName() + " " + student.getStudentLastName() + " " + student.getRosterName()
+        var name = ""
+        if (indexPath.row >= numberOfNonSignedOut) {
+            name = student.getStudentFirstName() + " " + student.getStudentLastName() + " " + student.getRosterName() + " signed out!"
+        } else {
+            name = student.getStudentFirstName() + " " + student.getStudentLastName() + " " + student.getRosterName()
+        }
+        cell.textLabel?.text = name
         return cell
     }
 
@@ -61,12 +81,14 @@ class TodayRosterViewController: UIViewController, UITableViewDataSource, UITabl
     }
 
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        let student = studentList[(indexPath.row)]
-        forwardedStudentID = student.getStudentID()
-        forwardedRosterID = student.getRosterID()
-        forwardedStudentFirstName = student.getStudentFirstName()
-        forwardedStudentLastName = student.getStudentLastName()
-        performSegueWithIdentifier("TodayRosterToEditAttendance", sender: self)
+        if (indexPath.row < numberOfNonSignedOut) {
+            let student = studentList[(indexPath.row)]
+            forwardedStudentID = student.getStudentID()
+            forwardedRosterID = student.getRosterID()
+            forwardedStudentFirstName = student.getStudentFirstName()
+            forwardedStudentLastName = student.getStudentLastName()
+            performSegueWithIdentifier("TodayRosterToEditAttendance", sender: self)
+        }
     }
 
     @IBAction func returnToTodayRosterUnwind(segue: UIStoryboardSegue) {
