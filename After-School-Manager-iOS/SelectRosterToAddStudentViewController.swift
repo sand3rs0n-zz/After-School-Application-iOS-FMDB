e1ac3cbc50d74fdd4eb92d79d953950985ecd5e9
Stephanie Cruz <scruz2294@ufl.edu>
2016-04-12 11:36:20 -0400

    added dates to roster from edit student

--- After-School-Manager-iOS/SelectRosterToAddStudentViewController.swift
+++ After-School-Manager-iOS/SelectRosterToAddStudentViewController.swift
@@ -29,7 +29,18 @@ class SelectRosterToAddStudentViewController: UIViewController, UITableViewDataS
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let roster = selectRosterToAddStudentModel.getRoster(indexPath.row)
         let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
-        cell.textLabel?.text = roster.getName()
+        let name = roster.getName()
+        let startDay = roster.getStartDay()
+        let startMonth = roster.getStartMonth()
+        let startYear = roster.getStartYear()
+        let endDay = roster.getEndDay()
+        let endMonth = roster.getEndMonth()
+        let endYear = roster.getEndYear()
+        let date = "\(startMonth)/\(startDay)/\(startYear) - \(endMonth)/\(endDay)/\(endYear)"
+        cell.textLabel?.text = "\(name)"
+        
+        cell.detailTextLabel?.text = "\(date)"
+        cell.detailTextLabel?.textAlignment = NSTextAlignment.Right
         return cell
     }
 
